c2ac5ee5b06ced035ffb1c75c63a94a952bc5c03
AnnaCherdan <100855612+AnnaCherdan@users.noreply.github.com>
2022-07-30 15:51:26 +0700

    Win 8

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import time
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 
@@ -16,7 +19,12 @@ def chrome_options(chrome_options):
 def driver_friends_chrome(request):
     drifriend = webdriver.Chrome()
     drifriend.get('https://petfriends.skillfactory.ru/login')
+    WebDriverWait(drifriend, 10).until(EC.presence_of_element_located((By.ID, "email")))
     drifriend.find_element_by_xpath('//input[@id="email"]').send_keys('anchetest@mail.ru')
     drifriend.find_element_by_xpath('//*[@id="pass"]').send_keys('ljkmvtyf')
+    WebDriverWait(drifriend, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR,
+                                                                           'button[type="submit"]')))
     drifriend.find_element_by_xpath('//body/div[1]/div[1]/form[1]/div[3]/button[1]').submit()
     return drifriend
+
+    drifriend.quit()
